dfef8cb7f60ec6548c2918f8f512c05f95d8f24a
LeoAsher25 <leoasher2507@gmail.com>
2021-05-01 17:37:13 +0700

    up to contact

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+var typed = new Typed(".typing", {
+    strings: ["The most blessed lion🙂", "PTITer", ],
+    typeSpeed: 60,
+    backSpeed: 20,
+    loop: true
+});
+
+
+var typed = new Typed(".typing-1", {
+    strings: ["Who am I?", "How well do you know about me?"],
+    typeSpeed: 60,
+    backSpeed: 20,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["What I like?"],
+    typeSpeed: 60,
+    backSpeed: 20,
+    loop: true
+});
+
+var typed = new Typed(".typing-3", {
+    strings: ["How to contact me?"],
+    typeSpeed: 60,
+    backSpeed: 20,
+    loop: true
+});
+
+var typed = new Typed(".typing-4", {
+    strings: ["Nguyễn Văn Hậu", "Leo Asher"],
+    typeSpeed: 60,
+    backSpeed: 20,
+    loop: true
+});
