96125e5fc2206d70a4745311328401847ae2f48d
LeoAsher25 <leoasher2507@gmail.com>
2021-05-05 23:16:01 +0700

    dark mode3

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,26 +6,7 @@ var typed = new Typed(".typing", {
 });
 
 
-var typed = new Typed(".typing-1", {
-    strings: ["Who am I?", "How well do you know about me?"],
-    typeSpeed: 60,
-    backSpeed: 20,
-    loop: true
-});
 
-var typed = new Typed(".typing-2", {
-    strings: ["What I like?"],
-    typeSpeed: 60,
-    backSpeed: 20,
-    loop: true
-});
-
-var typed = new Typed(".typing-3", {
-    strings: ["How to contact me?"],
-    typeSpeed: 60,
-    backSpeed: 20,
-    loop: true
-});
 
 var typed = new Typed(".typing-4", {
     strings: ["Nguyễn Văn Hậu", "Leo Asher"],
