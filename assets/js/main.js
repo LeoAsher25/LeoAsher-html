a50ec824c9912dd8048aee2ce41233239d4d97a8
LeoAsher25 <leoasher2507@gmail.com>
2021-05-09 13:54:43 +0700

    devide folder 1

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+var typed = new Typed(".typing", {
+    strings: ["The most blessed lion🙂", "Coder", ],
+    typeSpeed: 60,
+    backSpeed: 20,
+    loop: true
+});
+
+
+
+
+var typed = new Typed(".typing-4", {
+    strings: ["Nguyễn Văn Hậu", "Leo Asher"],
+    typeSpeed: 60,
+    backSpeed: 20,
+    loop: true
+});
